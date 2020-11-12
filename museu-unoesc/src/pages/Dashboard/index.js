e8381c1b1e7ab942f79aabe1a12e9f2f6368ca80
HitoriSora <juniorfercos@gmail.com>
2020-11-11 21:01:43 -0300

    Ajustes tela inicial.

--- museu-unoesc/src/pages/Dashboard/index.js
+++ museu-unoesc/src/pages/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
 
 import {
   Container,
@@ -7,11 +8,23 @@ import {
   TituloHeader,
   PesquisaContainer,
   LoginContaienr,
+  Content,
+  Categoria,
+  PesquisaCategoria,
+  CategoriaItem,
+  IconeSearch,
 } from "./styles";
 
 import logoUnoesc from "../../assets/unoescLogo.jpg";
 
 const Dashboard = () => {
+  const categorias = [
+    { id: 1, descricao: "Ciencias" },
+    { id: 2, descricao: "Ciencias" },
+    { id: 3, descricao: "Ciencias" },
+    { id: 4, descricao: "Ciencias" },
+    { id: 5, descricao: "Ciencias" },
+  ];
   return (
     <Container>
       <Header>
@@ -21,6 +34,20 @@ const Dashboard = () => {
         <PesquisaContainer />
         <LoginContaienr>Login</LoginContaienr>
       </Header>
+      <Content>
+        <Categoria>
+          <PesquisaCategoria>
+            Categoria
+            <IconeSearch>
+              <FiSearch size={20} />
+            </IconeSearch>
+          </PesquisaCategoria>
+
+          {categorias.map((e) => (
+            <CategoriaItem key={e.id}>{e.descricao}</CategoriaItem>
+          ))}
+        </Categoria>
+      </Content>
     </Container>
   );
 };
